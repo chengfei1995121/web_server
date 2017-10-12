8658c7a7280a2fe489a6bf6ba86229664c54c486
chengfei1995121 <chengfei1995121@gmail.com>
2017-10-12 20:27:57 +0800

    file i/o

--- web_server.cpp
+++ web_server.cpp
@@ -13,6 +13,9 @@
 #include<signal.h>
 #include<sys/wait.h>
 #include<sys/stat.h>
+#include<sys/mman.h>
+#include<sys/fcntl.h>
+#include<sys/stat.h>
 using namespace std;
 //获取请求文件名
 void getfilename(char a[],char filename[])
@@ -54,9 +57,12 @@ void get_type(char filename[], char type[])
 	}
 }
 //响应
-void respond(int n,char filename[])
+void respond(int n,char filename[],int filesize)
 {
-	ifstream cf(filename,std::ios::binary);
+	//cout<<"1:"<<filesize<<endl;
+	char type[1000];
+	//将文件全部读到内存中
+	/*ifstream cf(filename,std::ios::binary);
 	char a[1000000];
 	char type[1000];
 	int chang;
@@ -64,17 +70,25 @@ void respond(int n,char filename[])
 	chang = cf.tellg();
 	cf.seekg(0, ios::beg);
 	cf.read(a, 1000000);
-	//cout << strlen(a) << endl;
+	cout <<"2:"<< strlen(a) << endl;*/
+	//文件映射
+	int stcd=open(filename,O_RDONLY,0);
+	char *srcp;
+	srcp=static_cast<char*>(mmap(0,filesize,PROT_READ,MAP_PRIVATE,stcd,0));
+	close(stcd);
+	//munmap(srcp,filesize);
 	get_type(filename, type);
 	char b[1000] = "http/1.0 200 ok\r\nAccept-Ranges:bytes\r\nServer:cf web server\r\nContent-type:";
 	strcat(b, type);
 	
 	strcat(b, "\r\n\r\n");
 	write(n, b, strlen(b));
-	int slen = 0;
-	slen=send(n,a,chang,0);
-/*	cout << "sizeof:" << chang << endl;
-	cout <<"leng:"<< cf.tellg() << endl;
+	//int slen = 0;
+	write(n,srcp,filesize);
+	munmap(srcp,filesize);
+	//slen=send(n,a,chang,0);
+	//cout << "sizeof:" << chang << endl;
+	/*cout <<"leng:"<< cf.tellg() << endl;
 	cout << "slen:" << slen << endl;*/
 }
 //错误处理
@@ -95,6 +109,7 @@ void handle_request(int n)
 	strcat(filename,"./test");
 	getfilename(a, filename);//获取文件名
 	//cout<<filename<<endl;
+	//假如文件不存在，返回错误 stat函数用于读取文件信息
 	if(stat(filename,&sbuf)<0)	
 	{
 		clienterror(n);
@@ -102,7 +117,7 @@ void handle_request(int n)
 		
 	}
 	//cout << filename << endl;
-	respond(n,filename);//响应
+	respond(n,filename,sbuf.st_size);//响应
 }
 //信号处理器
 void handler(int sign)
