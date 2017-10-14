278eb6109aaf7b8e11ff150746550535865b2a81
chengfei1995121 <chengfei1995121@gmail.com>
2017-10-14 16:46:18 +0800

    响应错误页面

--- web_server.cpp
+++ web_server.cpp
@@ -94,8 +94,10 @@ void respond(int n,char filename[],int filesize)
 //错误处理
 void clienterror(int fd)
 {
-	char buf[1000]={"HTTP/1.0 404 NOT FOUND\r\n\r\n"};
+	char buf[1000]={"HTTP/1.0 404 NOT FOUND\r\nAccept-ranges:bytes\r\nContent-type:text/html\r\n\r\n"};
 	write(fd,buf,strlen(buf));
+	char a[1000]={"<html><head></head><body><p style='font-size:20px;'>404</p> NOT FOUND</body></html>"};
+	write(fd,a,strlen(a));
 }
 //处理请求
 void handle_request(int n)
