7cdbbec7beb9aced11b847f9ad82e252064ec828
chengfei1995121 <chengfei1995121@gmail.com>
2017-10-22 18:04:31 +0800

    fix respond

--- respond.cpp
+++ respond.cpp
@@ -2,6 +2,7 @@
 #include "respond.h"
 #include "handle_file.h"
 #include "handle_request.h"
+#define B_SIZE 10000
 using namespace std;
 void respond(struct request_header *RH)
 {
@@ -16,21 +17,31 @@ void respond(struct request_header *RH)
 	chang = cf.tellg();
 	cf.seekg(0, ios::beg);
 	cf.read(a, 1000000);
-	cout <<"2:"<< strlen(a) << endl;*/
+	cout <<"2:"<< strlen(a) << endl;
+	*/
+	respond_header(RH);
+	respond_body(RH);
+
+	//slen=send(n,a,chang,0);
+	//cout << "sizeof:" << chang << endl;
+	/*cout <<"leng:"<< cf.tellg() << endl;
+	cout << "slen:" << slen << endl;*/
+}
+//返回头部消息
+void respond_header(struct request_header *RH)
+{
+	char buf[B_SIZE]="http/1.0 200 ok\r\nAccept-Ranges:bytes\r\nServer:cf web server\r\nContent-type:";
+	sprintf(buf,"%s%s\r\nContent-length:%d\r\n\r\n",buf,RH->filetype,RH->filesize);
+	write(RH->fd,buf,strlen(buf));
+}
+//返回主体内容
+void respond_body(struct request_header *RH)
+{
 	//文件映射
 	int stcd=open(RH->filename,O_RDONLY,0);
 	char *srcp;
 	srcp=static_cast<char*>(mmap(0,RH->filesize,PROT_READ,MAP_PRIVATE,stcd,0));
 	close(stcd);
-	char b[1000] = "http/1.0 200 ok\r\nAccept-Ranges:bytes\r\nServer:cf web server\r\nContent-type:";
-	strcat(b, RH->filetype);
-	strcat(b, "\r\n\r\n");
-	write(RH->fd, b, strlen(b));
-	//int slen = 0;
 	write(RH->fd,srcp,RH->filesize);
 	munmap(srcp,RH->filesize);
-	//slen=send(n,a,chang,0);
-	//cout << "sizeof:" << chang << endl;
-	/*cout <<"leng:"<< cf.tellg() << endl;
-	cout << "slen:" << slen << endl;*/
 }
--- respond.h
+++ respond.h
@@ -1,4 +1,6 @@
 #ifndef rs 
 #define rs 
 void respond(struct request_header *RH);
+void respond_header(struct request_header *RH);
+void respond_body(struct request_header *RH);
 #endif
