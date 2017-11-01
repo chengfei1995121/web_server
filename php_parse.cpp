5f9a526fff1f0172a9786b93fa5c05a5b3afba76
chengfei1995121 <chengfei1995121@gmail.com>
2017-11-01 21:05:51 +0800

    fix fastcgi

--- php_parse.cpp
+++ php_parse.cpp
@@ -1,5 +1,6 @@
 #include "php_parse.h"
 #include<arpa/inet.h>
+#include<malloc.h>
 FCGI_Header makerequestheader(int type,int requestId,int contentLength,int paddingLength)
 {
 	FCGI_Header header;
@@ -19,13 +20,13 @@ FCGI_BeginRequestBody makeBB(int role)
 	bb.roleB1=(unsigned char)((role>>8)&0xff);
 	bb.roleB0=(unsigned char)(role&0xff);
 	bb.flags=0;
-	memset(bb.reserved,sizeof(bb.reserved),0);
+	memset(bb.reserved,0,sizeof(bb.reserved));
 	return bb;
 }
 FCGI_BeginRequestRecord makerecord()
 {
 	FCGI_BeginRequestRecord BR;
-	BR.header=makerequestheader(1,1,sizeof(BR.header),0);
+	BR.header=makerequestheader(1,1,sizeof(BR.body),0);
 	BR.body=makeBB(1);
 	return BR;
 }
@@ -38,7 +39,7 @@ bool makeNameValueBody(char *name,int nameLen,char *value,int valueLen,unsigned 
 	}
 	else 
 	{
-			*bodyptr++=(unsigned char)((nameLen>>24) |0x80);
+		*bodyptr++=(unsigned char)((nameLen>>24) | 0x80);
 		*bodyptr++=(unsigned char)(nameLen>>16);
 		*bodyptr++=(unsigned char)(nameLen>>8);
 		*bodyptr++=(unsigned char)nameLen;
@@ -65,6 +66,98 @@ bool makeNameValueBody(char *name,int nameLen,char *value,int valueLen,unsigned 
 	*bodylenptr=bodyptr-start;
 	return true;
 }
+void sendparme(int fd,char *name,char *value)
+{
+	FCGI_Header header;
+	char request[1000];
+	int bufflen=0;
+	//int n=write(fd,(char *)&header,sizeof(header));
+	//memcpy(request,(char *)&header,8);
+	unsigned char buff[1000];
+	bzero(buff,sizeof(buff));
+	makeNameValueBody(name,strlen(name),value,strlen(value),buff,&bufflen);
+	//n=write(fd,buff,bufflen);
+	header=makerequestheader(4,1,bufflen,0);
+	//memcpy(request,(char *)&header,8);
+	//memcpy(request+8,buff,bufflen);
+	//printf("字符 %s\n",request);
+	//int n=write(fd,(char *)&header,sizeof(header));
+	write(fd,(char *)&header,8);
+	int m=write(fd,(char *)&buff,bufflen);
+	//printf("字符 %s\n",request);
+	printf("\n %d %s\n",m,buff);
+	return;
+}
+/*int sendParamsRecord(
+        int fd,
+        char *name,
+        int nlen,
+        char *value,
+        int vlen)
+{
+    unsigned char *buf, *old;
+    int ret, pl,  cl = nlen + vlen;
+    cl = (nlen < 128) ? ++cl : cl + 4; 
+    cl = (vlen < 128) ? ++cl : cl + 4; 
+
+    // 计算填充数据长度
+    pl = (cl % 8) == 0 ? 0 : 8 - (cl % 8);
+    old = buf = (unsigned char *)malloc(8 + cl + pl);
+
+    FCGI_Header nvHeader = makerequestheader(4, 1, cl, pl);
+    memcpy(buf, (char *)&nvHeader, 8);
+    buf = buf + 8;
+
+    if (nlen < 128) { // name长度小于128字节，用一个字节保存长度
+        *buf++ = (unsigned char)nlen;
+    } else { // 大于等于128用4个字节保存长度
+        *buf++ = (unsigned char)((nlen >> 24) | 0x80);
+        *buf++ = (unsigned char)(nlen >> 16);
+        *buf++ = (unsigned char)(nlen >> 8);
+        *buf++ = (unsigned char)nlen;
+    }
+
+    if (vlen < 128) { // value长度小于128字节，用一个字节保存长度
+        *buf++ = (unsigned char)vlen;
+    } else { // 大于等于128用4个字节保存长度
+        *buf++ = (unsigned char)((vlen >> 24) | 0x80);
+        *buf++ = (unsigned char)(vlen >> 16);
+        *buf++ = (unsigned char)(vlen >> 8);
+        *buf++ = (unsigned char)vlen;
+    }
+
+    memcpy(buf, name, nlen);
+    buf = buf + nlen;
+    memcpy(buf, value, vlen);
+
+    ret = write(fd, old, 8 + cl + pl);
+
+    free(old);
+
+    if (ret == (8 + cl + pl)) {
+        return 0;
+    } else {
+        return -1;
+    }
+}*/
+void makeendrequest(int fd)
+{
+	FCGI_Header endrecord;
+	endrecord=makerequestheader(4,1,0,0);
+	write(fd,(char *)&endrecord,sizeof(endrecord));
+}
+void printf_html(char *context)
+{
+	for(;*context!='\0';context++)
+	{
+		if(*context=='<')
+		{
+			while(*context!='\0')
+				printf("%c",*context);
+			break;
+		}
+	}
+}
 int main()
 {
 	int fd;
@@ -78,7 +171,6 @@ int main()
 	serveraddr.sin_family=AF_INET;
 	serveraddr.sin_port=htons(9000);
 	serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-	char b[100];
 	//printf("2\n");
 	int confd=connect(fd,(sockaddr *)&serveraddr,sizeof(serveraddr));
 	//printf("3\n");
@@ -90,17 +182,30 @@ int main()
 	else 
 	{
 			printf("失败 %d",confd);
-			//exit(-1);
+			exit(-1);
 	}
 	FCGI_BeginRequestRecord record=makerecord();
 	FCGI_Header header;
 	int l=write(fd,(char *)&record,sizeof(record));
-	printf("写入的长度 %d %d\n",l,sizeof(record));
-	int n=read(fd,(char *)&header,100);
+	char a[100]={"SCRIPT_FILENAME"};
+	char b[100]={"/home/chengfei/index.php"};
+	char c[100]={"REQUEST_METHOD"};
+	char d[100]={"GET"};
+	sendparme(fd,a,b);
+	sendparme(fd,c,d);
+	makeendrequest(fd);
+	int n=read(fd,&header,sizeof(header));
+	char context[1000];
+	printf("类型 %d\n",header.type);
 	if(n>0)
-	{printf("长度 %d",n);
-	b[n]='\0';
-	printf("%s",b);
+	{
+		if(header.type==6){
+		int conlen=(header.contentLengthB1>>8)+header.contentLengthB0;
+		printf("获取内容的长度 %d\n",conlen);
+		read(fd,context,conlen);
+		printf("%s\n",context);
+		//printf_html(context);
+		}
 	}
 	else 
 	{
